54a9f73d6dfc1583d673defb07a10e48fa9aa20d
Michael Cheers <michaelcheers8@gmail.com>
2016-05-08 14:58:35 +0200

    Added Spanish.

---
+++ Number Sayer Bridge/HTML.cs
@@ -0,0 +1,63 @@
+﻿using Bridge;
+using Bridge.Html5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Sayer_Bridge
+{
+    [FileName("html.js")]
+    static class HTML
+    {
+        static SelectElement voice { get { return Document.GetElementById<SelectElement>("voice"); } }
+        static SelectElement language { get { return Document.GetElementById<SelectElement>("language"); } }
+        static NumberSayer.Language currentLanguage { get{ return (NumberSayer.Language)language.SelectedIndex; } }
+        static string currentVoice { get { return voice.Value; } }
+
+        [Ready]
+        static void Start ()
+        {
+            Document.GetElementById<ButtonElement>("submit").OnClick = Submit;
+            foreach (NumberSayer.Language item in Enum.GetValues(typeof(NumberSayer.Language)))
+                language.AppendChild(new OptionElement
+                {
+                    Value = item.ToString(),
+                    InnerHTML = item.ToString()
+                });
+            language.SelectedIndex = 0;
+            Update();
+        }
+
+        static Dictionary<string, NumberSayer> sayers = new Dictionary<string, NumberSayer>();
+
+        static void Submit(MouseEvent<ButtonElement> arg)
+        {
+            var key = currentVoice + currentLanguage.ToString();
+            NumberSayer sayer;
+            if (sayers.ContainsKey(key))
+                sayer = sayers[key];
+            else
+                sayer = (sayers[key] = new NumberSayer(currentLanguage, currentVoice));
+            sayer.Say(new BigInteger(Document.GetElementById<InputElement>("number").Value)).Play();
+        }
+
+        static void Update ()
+        {
+            voice.InnerHTML = "";
+            var currentKnownVoices = NumberSayer.knownVoices[(NumberSayer.Language)language.SelectedIndex];
+            foreach (var item in currentKnownVoices)
+                voice.AppendChild(new OptionElement
+                {
+                    Value = item.ToString(),
+                    InnerHTML = item.ToString()
+                });
+            voice.AppendChild(new OptionElement
+            {
+                Value = "mixed",
+                InnerHTML = "Mixed"
+            });
+        }
+    }
+}
--- Number Sayer Bridge/NumberSayer.cs
+++ Number Sayer Bridge/NumberSayer.cs
@@ -12,40 +12,81 @@ namespace Number_Sayer_Bridge
         public Sound[] smalls;
         public string Voice;
 
-        public NumberSayer(string voice = "Michael")
+        public NumberSayer(Language language = Language.English, string voice = "Michael")
         {
+            this.language = language;
             Voice = voice;
-            smalls = new Sound[]{
-            LoadSound("0"),
-            LoadSound("1"),
-            LoadSound("2"),
-            LoadSound("3"),
-            LoadSound("4"),
-            LoadSound("5"),
-            LoadSound("6"),
-            LoadSound("7"),
-            LoadSound("8"),
-            LoadSound("9"),
-            LoadSound("10"),
-            LoadSound("11"),
-            LoadSound("12")
-        };
-            thir = LoadSound("thir");
-            fif = LoadSound("fif");
-            hundred = LoadSound("hundred");
-            and = LoadSound("and");
-            ty = LoadSound("ty");
+            switch (language)
+            {
+                case Language.English:
+                    {
+                        smalls = new Sound[]
+                        {
+                            LoadSound("0"),
+                            LoadSound("1"),
+                            LoadSound("2"),
+                            LoadSound("3"),
+                            LoadSound("4"),
+                            LoadSound("5"),
+                            LoadSound("6"),
+                            LoadSound("7"),
+                            LoadSound("8"),
+                            LoadSound("9"),
+                            LoadSound("10"),
+                            LoadSound("11"),
+                            LoadSound("12")
+                        };
+                        break;
+                    }
+                case Language.Spanish:
+                    {
+                        smalls = new Sound[]
+                        {
+                            LoadSound("0"),
+                            LoadSound("1"),
+                            LoadSound("2"),
+                            LoadSound("3"),
+                            LoadSound("4"),
+                            LoadSound("5"),
+                            LoadSound("6"),
+                            LoadSound("7"),
+                            LoadSound("8"),
+                            LoadSound("9"),
+                            LoadSound("10"),
+                            LoadSound("11"),
+                            LoadSound("12"),
+                            LoadSound("13"),
+                            LoadSound("14"),
+                            LoadSound("15")
+                        };
+                        break;
+                    }
+                default:
+                    break;
+            }
         }
 
-        public readonly Sound thir;
-        public readonly Sound fif;
-        public readonly Sound hundred;
-        public readonly Sound and;
-        public readonly Sound ty;
+        public Sound thir { get { return LoadSound("thir"); } }
+        public Sound fif { get { return LoadSound("fif"); } }
+        public Sound and { get { return LoadSound("and"); } }
+        public Sound ty { get { return LoadSound("ty"); } }
         public readonly Random rnd = new Random();
         public static readonly Dictionary<Language, string[]> knownVoices = new Dictionary<Language, string[]>
         {
-            {Language.English, new[] {"Ally", "Ben", "Jeff", "Laurie", "Melissa", "Michael"} }
+            {Language.English, new[] {"Ally", "Ben", "Jeff", "Laurie", "Melissa", "Michael", "Seamus"} },
+            {Language.Spanish, new[] {"Ana"} }
+        };
+
+        public static readonly Dictionary<Language, int> irregularStarters = new Dictionary<Language, int>
+        {
+            {Language.English, 13 },
+            {Language.Spanish, 16 }
+        };
+
+        public static readonly Dictionary<Language, int> numberScale = new Dictionary<Language, int>
+        {
+            {Language.English, 1000 },
+            {Language.Spanish, 1000000 }
         };
 
         public Dictionary<string, Sound> alreadyDone = new Dictionary<string, Sound>();
@@ -55,7 +96,7 @@ namespace Number_Sayer_Bridge
             if (alreadyDone.ContainsKey(value))
                 return alreadyDone[value];
             AudioElement[] mixedResult = new AudioElement[] { };
-            string format = "Sounds/{0}/{1}.wav";
+            string format = "Sounds/" + language.ToString() + "/{0}/{1}.wav";
             if (Voice == "mixed")
                 foreach (var item in knownVoices[language])
                     mixedResult.Push(new AudioElement(string.Format(format, item, value)));
@@ -93,77 +134,156 @@ namespace Number_Sayer_Bridge
         public Sound Say(Number value)
         {
             Sound result = new Sound();
-            if (value < 1000)
+            if (value < 1000000)
             {
-                if (value < 100)
+                if (value < 1000)
                 {
-                    if (value < 30)
+                    if (value < 100)
                     {
                         if (value < 20)
                         {
-                            if (value < 13)
+                            if (value < irregularStarters[language])
                             {
                                 result.AppendThis(smalls[(int)value]);
                                 return result;
                             }
-                            result.AppendThis(GetThirFifSound(value % 10));
-                            result.AppendThis(LoadSound("teen"));
-                            return result;
+                            switch (language)
+                            {
+                                case Language.English:
+                                    {
+                                        result.AppendThis(GetThirFifSound(value % 10));
+                                        result.AppendThis(LoadSound("teen"));
+                                        return result;
+                                    }
+                                default:
+                                    break;
+                            }
                         }
-                        result.AppendThis(LoadSound("20"));
-                        var dig22 = value % 10;
-                        if (dig22 != 0)
-                            result.AppendThis(Say(dig22));
+                        var dig1 = value / 10;
+                        var dig2 = value % 10;
+                        switch (language)
+                        {
+                            case Language.English:
+                                {
+                                    if (dig1 == 2)
+                                        result.AppendThis(LoadSound("20"));
+                                    else
+                                    {
+                                        result.AppendThis(GetThirFifSound(dig1));
+                                        result.AppendThis(ty);
+                                    }
+                                    break;
+                                }
+                            case Language.Spanish:
+                                {
+                                    result.AppendThis(LoadSound(dig1 + "0"));
+                                    if (dig2 != 0)
+                                        result.AppendThis(and);
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+                        if (dig2 != 0)
+                            result.AppendThis(Say(dig2));
                         return result;
                     }
-                    var dig1 = value / 10;
-                    var dig2 = value % 10;
-                    result.AppendThis(GetThirFifSound(dig1));
-                    result.AppendThis(ty);
-                    if (dig2 != 0)
-                        result.AppendThis(Say(dig2));
+
+                    int hundred = (int)(value / 100);
+                    var remainder = value % 100;
+                    switch (language)
+                    {
+                        case Language.English:
+                            {
+                                result.AppendThis(Say(hundred));
+                                result.AppendThis(LoadSound("hundred"));
+                                break;
+                            }
+                        case Language.Spanish:
+                            {
+                                switch (hundred)
+                                {
+                                    case 1:
+                                        {
+                                            result.AppendThis(LoadSound("100"));
+                                            break;
+                                        }
+                                    case 5:
+                                        {
+                                            result.AppendThis(LoadSound("500"));
+                                            break;
+                                        }
+                                    default:
+                                        {
+                                            result.AppendThis(Say(hundred));
+                                            result.AppendThis(LoadSound("hundred"));
+                                            break;
+                                        }
+                                }
+                                break;
+                            }
+                        default:
+                            break;
+                    }
+                    if (remainder != 0)
+                    {
+                        if (language == Language.English)
+                            result.AppendThis(and);
+                        result.AppendThis(Say(remainder));
+                    };
                     return result;
                 }
-
-                var hundred = value / 100;
-                var remainder = value % 100;
-                result.AppendThis(Say(hundred));
-                result.AppendThis(this.hundred);
-                if (remainder != 0)
+                switch (language)
                 {
-                    result.AppendThis(and);
-                    result.AppendThis(Say(remainder));
+                    case Language.Spanish:
+                        {
+                            var part1 = value / 1000;
+                            var part2 = value % 1000;
+                            if (part1 != 1)
+                                result.AppendThis(Say(part1));
+                            result.AppendThis(LoadSound("thousand"));
+                            if (part2 != 0)
+                                result.AppendThis(Say(part2));
+                            return result;
+                        }
+                    default:
+                        break;
                 }
-                return result;
             }
             Number current = 1;
             int n = 0;
-            for (; value >= current; n++, current *= 1000) ;
+            Number languageNumberScale = numberScale[language];
+            for (; value >= current; n++, current *= languageNumberScale) ;
             n -= 2;
-            current /= 1000;
+            current /= languageNumberScale;
             while (true)
             {
                 var condition = n == -1;
-                var currentVal = (value / current) % 1000;
+                var currentVal = (value / current) % languageNumberScale;
                 if (currentVal != 0)
                 {
-                    if (currentVal < 100 && condition)
+                    if (currentVal < 100 && condition && language == Language.English)
                         result.AppendThis(and);
                     result.AppendThis(Say(currentVal));
                     if (!condition)
-                        result.AppendThis(LoadSound(placeValues[n]));
+                    {
+                        result.AppendThis(LoadSound(languageNumberScale == 1000 ? placeValues[n] : placeValues[n + 1]));
+                        if (language == Language.Spanish && currentVal != 1)
+                            result.AppendThis(LoadSound("es"));
+                    }
                 }
-                current /= 1000;
+                current /= languageNumberScale;
                 n--;
                 if (current == 0)
                     return result;
             }
         }
 
-        static string[] placeValues = new string[] { "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sedecillion" };
+        static string[] placeValues = new string[] { "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion", "quindecillion", "sedecillion", "septendecillion", "octodecillion", "novendecillion", "vigintillion" };
         public enum Language
         {
-            English
+            English,
+            Spanish
         }
     }
 }

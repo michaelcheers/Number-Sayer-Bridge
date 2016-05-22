29ddec559dbd2c260e3df34bb4abffd0d38d58ce
Michael Cheers <michaelcheers8@gmail.com>
2016-05-22 09:04:39 +0200

    Removed new keyword in BigInteger.

--- Number Sayer Bridge/BigInteger.cs
+++ Number Sayer Bridge/BigInteger.cs
@@ -8,6 +8,7 @@ using GenNum = Bridge.Any<int, BigInteger>;
 
 [External]
 [Name("bigInt")]
+[Constructor("bigInt")]
 #pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
 #pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
 public class BigInteger

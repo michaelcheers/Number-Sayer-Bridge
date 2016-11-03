51720ebead653296c21ce5c7df56405590767ddd
Michael Cheers <michaelcheers@users.noreply.github.com>
2016-11-03 14:55:10 +0100

    First test.

---
+++ Number Sayer Bridge/tests.js
@@ -0,0 +1,8 @@
+/// <reference path="Bridge/output/number_Sayer_Bridge.js" />
+
+test("Say 1000000", function ()
+{
+   var sayer = new NumberSayer();
+   var sound = sayer.say$2("1000000").sound.map(function(v){return v.name});
+   equal(sound[0], "1");
+});

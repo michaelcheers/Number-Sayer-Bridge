7277a609557d0b806b8e33ff2ec63166457272d1
Michael Cheers <michaelcheers8@gmail.com>
2016-11-17 21:45:46 +0100

    Removed tests.

--- Number Sayer Bridge/tests.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="Bridge/output/number_Sayer_Bridge.js" />
-/// <reference path="Bridge/output/bridge.js" />
-
-test("Say 1000000", function ()
-{
-   var sayer = new NumberSayer();
-   var sound = sayer.say$2("1000000").sound.map(function(v){return v.name});
-   equal(sound[0], "1");
-});
